ece16cdb10d9adeae2ed3808500cc7374aa17579
Stephen Marz <sgm@utk.edu>
2026-05-30 16:31:31 -0400

    Look through files so that we can serve plain pages.

--- src/mime.rs
+++ src/mime.rs
@@ -1,6 +1,6 @@
-// ── MIME ─────────────────────────────────────────────────────────────────────
+use std::{fs::File, path::Path, io::Read};
 
-pub fn mime_type(path: &std::path::Path) -> &'static str {
+pub fn mime_type(path: &Path) -> &'static str {
     match path.extension().and_then(|e| e.to_str()) {
         Some("html") | Some("htm") => "text/html",
         Some("css") => "text/css",
@@ -22,7 +22,22 @@ pub fn mime_type(path: &std::path::Path) -> &'static str {
         Some("woff") => "font/woff",
         Some("woff2") => "font/woff2",
         Some("ttf") => "font/ttf",
-        _ => "application/octet-stream",
+        _ => determine_mime_type(path),
+    }
+}
+
+fn determine_mime_type(path: &Path) -> &'static str {
+    if let Ok(fl) = File::open(path) {
+        for b in fl.bytes() {
+            let byte = b.unwrap_or(0);
+            if !byte.is_ascii() {
+                return "application/octet-stream";
+            }
+        }
+        "text/plain"
+    }
+    else {
+        "application/octet-stream"
     }
 }
 
