ea8f87b653aa8350175e6ce25be8970bb8cf5a5b
Stephen Marz <sgm@utk.edu>
2026-06-05 19:10:41 -0400

    Always canonicalize ROOT

--- src/main.rs
+++ src/main.rs
@@ -15,22 +15,12 @@ use std::{process, fs};
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     let mut args = Args::parse();
-    // HACK: The safe resolver pops . out of the PathBuf, leaving an empty
-    // pathbuf, which causes nothing to match when it is resolved.
-    if args.root.as_os_str() == "." {
-        match fs::canonicalize(args.root) {
-            Ok(c) => {
-                args.root = c;
-            }
-            Err(x) => {
-                eprintln!("Unable to canonicalize serve directory: {}", x);
-                process::exit(1);
-            }
+    args.root = match fs::canonicalize(args.root) {
+        Ok(c) => c,
+        Err(x) => {
+            eprintln!("Unable to canonicalize serve directory: {}", x);
+            process::exit(1);
         }
-    }
-    else if args.root.to_string_lossy().contains("..") {
-        eprintln!("Root directory cannot contain '..' for security reasons. Use absolute paths.");
-        process::exit(1);
-    }
+    };
     server::run(args).await;
 }
